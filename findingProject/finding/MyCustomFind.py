6f64a3eb5036a407dca6e663fcb1f8466a1b4965
Микола Чепіль <colyachepil@gmail.com>
2021-10-26 22:07:52 +0300

    Finding program

--- findingProject/finding/MyCustomFind.py
+++ findingProject/finding/MyCustomFind.py
@@ -3,31 +3,67 @@ import argparse#Бібліотека для роботи з консольним
 import filecmp#Бібліотека для порівняння файлів і каталогів
 import time#Бібліотека для роботи з часом
 
-parser = argparse.ArgumentParser()#Створення об'єкту, який відповідає за парсинг команд
 
-#Додання аргументів, які можна використовувати в командах
-parser.add_argument("-duplicates", action="store_true", help="insert duplicates(optional)")
-parser.add_argument("-large", action="store_true", help="insert files large than -size(optional)")
-parser.add_argument("-size", help="minimal size of insertions files(requaired after large argument)")
-parser.add_argument("-images", action="store_true", help="insert images(optional)")
-parser.add_argument("-old", action="store_true", help="insert files elder than one year(optional)")
-parser.add_argument("path", help="insertion path(requaired)")
-parser.add_argument("-o", help="file path where result will be saved saved(optional)")
+def parse_args(args):
+    parser = argparse.ArgumentParser()#Створення об'єкту, який відповідає за парсинг команд
 
-args = parser.parse_args()#Парсинг команди
+    #Додання аргументів, які можна використовувати в командах
+    parser.add_argument("-duplicates", action="store_true", help="insert duplicates(optional)")
+    parser.add_argument("-large", action="store_true", help="insert files large than -size(optional)")
+    parser.add_argument("-size", help="minimal size of insertions files(requaired after large argument)")
+    parser.add_argument("-images", action="store_true", help="insert images(optional)")
+    parser.add_argument("-old", action="store_true", help="insert files elder than one year(optional)")
+    parser.add_argument("path", help="insertion path(requaired)")
+    parser.add_argument("-o", help="file path where result will be saved saved(optional)")
 
-path = args.path+"\\"#Отриання шляху до заданої директорії
-
-files = os.listdir(path)#Отримання списку файлів в заданій директорії
+    return parser.parse_args(args)
 
 #Видалення не картинок зі списку файлів, якщо опція вказана в команді
-if args.images == True:
+def getImages(files):
     for file in files[:]:
         if not (file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png")):
            files.remove(file)
+    return files
+    
+#формування стрічки з результатом вибірки
+def getResult(files, path):
+    result = ""
+    for i in files:
+            result += path+i+"\n"
+    return result
+    
+#Запис результату вибірки у файл заданий користувачем, або вивід у консоль
+def writeResult(o, result):
+    if not o == None:
+        f = open(o, mode="w")
+        f.write(result)
+    else:
+        print(result)
+        
+def main():
+    args = parse_args(["-images", "D:\\InPc\\Downloads\\"])#Парсинг команди
+    path = args.path+"\\"#Отриання шляху до заданої директорії
+    files = os.listdir(path)#Отримання списку файлів в заданій директорії
+    if args.images:
+        files = getImages(files)
+    if args.duplicates:
+        files = getDuplicates(files, path)
+    if args.large:
+        files = getLarge(files, path, args.size)
+    if args.old:
+        files = getOld(files, path)
+    result = getResult(files, path)
+    writeResult(args.o, result)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
 
 #Пошук файлів дублікатів і видалення файлів зі списку, які ними не є, якщо це вказано в команді
-if args.duplicates == True:
+def getDuplicates(files, path):
     for i in files[:]:
         haveDupl = False
         for j in files[:]:
@@ -37,6 +73,9 @@ if args.duplicates == True:
                 haveDupl = True
         if haveDupl == False:
             files.remove(i)
+    return files
+
+
 
 #Отримання розміру файлу байтах
 def file_size(file_path):
@@ -69,25 +108,19 @@ def getFileSizeInByte(num, vel):
         return num*1024*1024*1024*1024
 
 #Видалення файлів зі списку, що мають менший розмір ніж вказаний користувачем, якщо задано командою
-if args.large == True:
+def getLarge(files, path, size):
     for i in files[:]:
-        if file_size(path+i) < getFileSize(args.size):
+        if file_size(path+i) < getFileSize(size):
             files.remove(i)
+    return files
 
 #Видалення файлів зі списку, якщо вони створені більше року тому і якщо це задано командою
-if args.old == True:
+def getOld(files, path):
     for i in files[:]:
         if time.time() - os.path.getctime(path+i) >= 86400*365 :
             files.remove(i)
+    return files
 
-#формування стрічки з результатом вибірки
-result = ""
-for i in files:
-        result += path+i+"\n"
 
-#Запис результату вибірки у файл заданий користувачем, або вивід у консоль
-if not args.o == None:
-    f = open(args.o, mode="w")
-    f.write(result)
-else:
-    print(result)
+
+
